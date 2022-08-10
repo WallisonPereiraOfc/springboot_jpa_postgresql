f1b4c5b29c3951410a6da39d730306f299ef8dd0
wallison Pereira <110045968+WallisonPereiraOfc@users.noreply.github.com>
2022-08-10 16:36:21 -0300

    Add files via upload

---
+++ springboot-rest-api-sample/src/main/java/br/com/springboot/curso_jdev_treinamento/Application.java
@@ -0,0 +1,28 @@
+package br.com.springboot.curso_jdev_treinamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+/**
+ *
+ * Spring Boot application starter class
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+    
+    @RequestMapping(value = "/olamundo/{nome}", method = RequestMethod.GET)
+    @ResponseStatus(HttpStatus.OK)
+    public String retorno(@PathVariable String nome) {
+    	
+    	return "ola mundo" + nome;
+    	
+    }
+}
